639e3f7ceb51e0a781f0ae9fdc1947b06310b485
imac <busuko@yahoo.com>
2023-10-31 12:35:21 +0200

    add templates, .env, .gitignore and url

---
+++ store/products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from products.views import *
+
+app_name = 'products'
+urlpatterns = [
+    path('', index, name='index'),
+    path('products/', products, name='products'),
+
+
+]
---
+++ store/products/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'products/index.html')
+
+def products(request):
+    return render(request, 'products/products.html')
